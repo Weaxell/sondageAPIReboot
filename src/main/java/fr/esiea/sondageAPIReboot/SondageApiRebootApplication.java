93db0c12901df4d17ceecfb28bfb2cb293f6a4b9
Weaxell <thomascarpentier5@gmail.com>
2020-12-18 22:06:07 +0100

    Ajout d'annoation pr doc swagger2 fonctionnelle et resultats de vote en liste de String

--- src/main/java/fr/esiea/sondageAPIReboot/SondageApiRebootApplication.java
+++ src/main/java/fr/esiea/sondageAPIReboot/SondageApiRebootApplication.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 
+@EnableSwagger2
 @SpringBootApplication
 public class SondageApiRebootApplication {
 
